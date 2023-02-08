55a47afa941e1372eb662ff24c42f28c924bf86e
Cody <110630101+CodySaal@users.noreply.github.com>
2023-02-08 09:52:34 -0600

    Adjusted required fields

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,26 +1,24 @@
 import { gql } from "@apollo/client"
 
 export const LOGIN_USER = gql`
-    mutation login($email: String, $password: String) {
+    mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
             token
             user {
                 _id
                 username
-                email
             }
         }
     }
 `
 
 export const ADD_USER = gql`
-    mutation addUser($username: String, $email: String, $password: String) {
+    mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
             token
             user {
                 _id
                 username
-                email
             }
         }
     }
@@ -32,6 +30,7 @@ export const SAVE_BOOK = gql`
             _id
             username
             email
+            bookCount
             savedBooks {
                 bookId
                 title
@@ -45,11 +44,10 @@ export const SAVE_BOOK = gql`
 `
 
 export const REMOVE_BOOK = gql`
-    mutation removeBook($bookId: bookId) {
+    mutation removeBook($bookId: String!) {
         removeBook(bookId: $bookId) {
             _id
             username
-            email
             savedBooks {
                 bookId
                 title
